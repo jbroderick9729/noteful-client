5c6ec5f42d551c26f0d1573f80fde0b5f4acce44
Mengqi Wang <wmq516@gmail.com>
2019-04-04 19:58:58 -0600

    finished AddNote POST api call

--- src/AddNote/AddNote.js
+++ src/AddNote/AddNote.js
@@ -4,32 +4,102 @@ import './AddNote.css'
 import AppContext from '../AppContext/AppContext'
 
 export default class AddNote extends Component {
+  constructor(props) {
+    super(props); 
+      this.state = {
+        name: '',
+        id: '',
+        modified: '',
+        content: '',
+        folderId: ''
+    }
+  }
+
   static contextType = AppContext
+
+  postNote = (e) => {
+    e.preventDefault();
+    const noteInfo = {
+      "id": this.state.id,
+      "name": this.state.name,
+      "modified": this.updateModified(),
+      "content": this.state.content,
+      "folderId": this.state.folderId
+    }
+
+    const url ='http://localhost:9090/notes'
+    const options = {
+      method: 'POST',
+      headers: {
+        'content-type': 'application/json'
+      },
+      body: JSON.stringify(noteInfo)
+    }
+
+    fetch(url, options)
+    .then(response => {
+      if (response.ok) {
+        return response.json()
+      }
+      throw new Error('Note failed to post!')
+    })
+    .then(alert('Note added!'))
+    .catch(err => console.log(err))
+  }
+
+
+  updateModified() {
+    const today = new Date();
+    const date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();  
+
+    return date;
+  }
+
+
+  updateName(name) {
+    this.setState(
+      { name,
+      id: name }
+    )
+  }
+
+  updateContent(content) {
+    this.setState({
+      content
+    })
+  }
+
+  updateFolderOption(folderId) {
+    this.setState({
+      folderId
+    })
+  }
+
 
   render() {
     const { folders } = this.context
     return (
       <section className='AddNote'>
         <h2>Create a note</h2>
-        <NotefulForm>
+        <NotefulForm onSubmit={e => this.postNote(e)}>
           <div className='field'>
             <label htmlFor='note-name-input'>
               Name
             </label>
-            <input type='text' id='note-name-input' />
+            <input type='text' id='note-name-input' onChange={e => this.updateName(e.target.value)}/>
           </div>
           <div className='field'>
             <label htmlFor='note-content-input'>
               Content
             </label>
-            <textarea id='note-content-input' />
+            <textarea id='note-content-input' onChange={e => this.updateContent(e.target.value)}/>
           </div>
           <div className='field'>
             <label htmlFor='note-folder-select'>
               Folder
             </label>
-            <select id='note-folder-select'>
-              <option value={null}>...</option>
+            <select id='note-folder-select' onChange={e => this.updateFolderOption(e.target.value)}>
+              <option value={null} >...</option>
               {folders.map(folder =>
                 <option key={folder.id} value={folder.id}>
                   {folder.name}
